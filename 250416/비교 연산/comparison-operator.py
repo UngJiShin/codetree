f4a0616bd0f3e042389746ded4b31be27c607559
JiUngShin <shinproto@gmail.com>
2025-04-16 15:26:21 +0900

    Title: 비교 연산, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250416/비교 연산/comparison-operator.py
+++ 250416/비교 연산/comparison-operator.py
@@ -1,4 +1,4 @@
-a, b = map(int,input().split())
+"""a, b = map(int,input().split())
 
 if a >= b:
     print(int(True))
@@ -29,3 +29,15 @@ if a != b:
     print(int(True))
 else:
     print(int(False))
+"""
+input = input()
+arr = input.split()
+a = int(arr[0])
+b = int(arr[1])
+
+print(int(a >= b))
+print(int(a > b))
+print(int(a <= b))
+print(int(a < b))
+print(int(a == b))
+print(int(a != b))
