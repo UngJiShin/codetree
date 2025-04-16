a3d5490a4d701a9a8c273d023c5bb30b2b661f94
JiUngShin <shinproto@gmail.com>
2025-04-16 15:19:36 +0900

    Title: 비교 연산, Time: 48ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250416/비교 연산/comparison-operator.py
@@ -0,0 +1,27 @@
+A, B = input().split()
+#print(A)
+#print(B)
+if A > B:
+    print(1)
+else:
+    print(0)
+if A >= B:
+    print(1)
+else:
+    print(0)
+if A <= B:
+    print(1)
+else:
+    print(0)
+if A < B:
+    print(1)
+else:
+    print(0)
+if A == B:
+    print(1)
+else:
+    print(0)
+if A != B:
+    print(1)
+else:
+    print(0)
